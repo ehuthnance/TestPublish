199785e82cdcade39254ddbe3e409036cbfb0029
Anonymous <84867210+ehuthnance@users.noreply.github.com>
2021-05-26 16:22:26 -0400

    Add files via upload

---
+++ resources/Scripts/Default/Scripts/ReturnToTop.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function(){
+   /* This is the jscript for the Back-To-Top icon that appears on the page layouts. It works in combination with the "a.go-to-top" Styles in our master style sheet (.css) */
+   
+	/* Check to see if the window is at top, if not then display button */
+   $(window).scroll(function(){
+      if ($(this).scrollTop() > 50) {
+         $('.go-to-top').fadeIn();
+      } else {
+         $('.go-to-top').fadeOut();
+      }
+   });
+   
+	/* Click event to scroll to top */
+   $('.go-to-top').click(function(){
+      
+	/* adjust speed - lower value faster */
+      $('html, body').animate({scrollTop: 0}, 400);
+      return false;
+   });
+});
---
+++ resources/Scripts/Default/Scripts/StickyHeaders.js
@@ -0,0 +1,12 @@
+﻿window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("stickyHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("h1");
+  } else {
+    header.classList.remove("h1");
+  }
+}
